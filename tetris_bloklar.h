08892304b2e8c730782dc5b9426c21654fe41ac6
Tolga <39439686+TolRed@users.noreply.github.com>
2019-01-04 21:38:42 +0300

    tetris_bloklar.h
    tetris_bloklar.h

---
+++ tetris_bloklar.h
@@ -0,0 +1,21 @@
+/*
+ * tetris_bloklar.h
+ *
+ *  Created on: 26 Ara 2018
+ *      Author: Tolga
+ */
+
+#ifndef TETRIS_BLOKLAR_H_
+#define TETRIS_BLOKLAR_H_
+
+#ifdef __cplusplus
+ extern "C" {
+#endif
+
+ void tetris_blok_yap(uint8_t no,uint8_t yon,uint8_t renk,uint8_t tip);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* TETRIS_BLOKLAR_H_ */
