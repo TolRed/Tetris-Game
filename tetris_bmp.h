609752115c489449b29c0d871649274ab398e91c
Tolga <39439686+TolRed@users.noreply.github.com>
2019-01-04 21:39:36 +0300

    tetris_bmp.h
    tetris_bmp.h

---
+++ tetris_bmp.h
@@ -0,0 +1,22 @@
+/*
+ * tetris_bmp.h
+ *
+ *  Created on: 29 Ara 2018
+ *      Author: Tolga
+ */
+
+#ifndef TETRIS_BMP_H_
+#define TETRIS_BMP_H_
+
+#ifdef __cplusplus
+ extern "C" {
+#endif
+
+ const char tetris_bmp[153606];
+
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif /* TETRIS_BMP_H_ */
